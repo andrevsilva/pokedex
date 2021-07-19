457657a755802cb9f293cbd1a3a79494921db952
andrevsilva <andre.silva@investtools.com.br>
2021-07-19 09:35:36 -0300

    mudando de class para hooks

--- src/components/pokemons/pokemonCard/pokemonCard.jsx
+++ src/components/pokemons/pokemonCard/pokemonCard.jsx
@@ -1,47 +1,46 @@
-import React , { Component } from 'react'
+import React , { useEffect , useState } from 'react'
 import '../index.css'
 import api from '../../../services/Api.js'
 import PokemonsInfo from '../pokemonsInfo/pokemonsInfo'
 
-class PokemonCard extends Component{
+export default function PokemonCard() {
+    const [pokemons , setPokemons ] = useState([]);
     
-    state = {
-        pokemons: null
-    }
 
-    async componentDidMount(){
-        const response = await api.get('')
-        const pokemons = response.data
-        this.setState({ pokemons: response.data['results'] })
-    }
+    useEffect(() => {
+        api.get('')
+            .then(response => {
+                setPokemons(response.data['results'])
+                
+               console.log(pokemons)
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+    }, [])
 
-    render(){
-
-        const { pokemons } = this.state
-
-        return (
-            <>
-                {this.state.pokemons ? (
-                    <div className="container">
-                        {pokemons.map(pokemon =>(
-                            <div className="card" key={pokemon.name}>
-                                
-                                <p>
-                                    <PokemonsInfo 
-                                        key={pokemon.name}
-                                        name={pokemon.name}
-                                        url={pokemon.url}
-                                    />
-                                </p>
-                            </div>
-                        ))}
-                    </div> 
-                ) : (
-                    <h1>Carregando os pokemons</h1>
-                )}
-            </>
-        )
-    }
+    return (
+        <>
+            {pokemons ? (
+                <div className="container">
+                    {pokemons.map(pokemon =>(
+                        <div className="card" key={pokemon.name}>
+                            
+                            <p>
+                                <PokemonsInfo 
+                                    key={pokemon.name}
+                                    name={pokemon.name}
+                                    url={pokemon.url}
+                                    pokemonIndex={pokemon.url.split('/')[pokemon.url.split('/').length - 2]}
+                                />
+                            </p>
+                        </div>
+                    ))}
+                </div> 
+            ) : (
+                <h1>Carregando os pokemons</h1>
+            )}
+        </>
+    )
+    
 }
-
-export default PokemonCard;
--- src/components/pokemons/pokemonsInfo/pokemonsInfo.jsx
+++ src/components/pokemons/pokemonsInfo/pokemonsInfo.jsx
@@ -1,35 +1,13 @@
 import React , { Component } from 'react'
 import '../index.css'
 
-class PokemonsInfo extends Component{
+ export default function PokemonsInfo(props) {
     
-    state = {
-        name: '',
-        imageUrl: '',
-        pokemonIndex: '',
-    }
-
-    componentDidMount(){
-        const { name , url } = this.props;
-        const pokemonIndex = url.split('/')[url.split('/').length - 2];
-        const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
-
-        this.setState({
-            name,
-            imageUrl,
-            pokemonIndex,
-        })
-    }
-
-    render(){
-        return (
+    return (
             <>
-                <span>{this.state.pokemonIndex}</span>
-                <h5>{this.state.name}</h5>
-                <img src={this.state.imageUrl} alt={this.state.name} />
+                <span>{props.pokemonIndex}</span>
+                <h5>{props.name}</h5>
+                <img src={`https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${props.pokemonIndex}.png?raw=true`} alt={props.name} />
             </>
         )
-    }
 }
-
-export default PokemonsInfo;
